a9ba7bd3e94bb73fe83f6a4655d140c03780d352
Rogoza Raluca <raluca_rogoza@yahoo.com>
2023-04-23 21:18:33 +0300

    added models for incomes and expenses

---
+++ api/src/main/java/com/budgettracker/api/models/Expense.java
@@ -0,0 +1,43 @@
+package com.budgettracker.api.models;
+
+import com.budgettracker.api.enums.Currency;
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name="expense")
+@Table(name="expenses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private BigInteger id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userCategory_id", nullable = false)
+    private UserCategory userCategory;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+    private String to;
+
+    @Column(nullable = false)
+    @NotBlank
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    @NotBlank
+    private Currency currency;
+
+    private LocalDateTime registeredAt;
+}
---
+++ api/src/main/java/com/budgettracker/api/models/Income.java
@@ -0,0 +1,46 @@
+package com.budgettracker.api.models;
+
+import com.budgettracker.api.enums.Currency;
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity(name="income")
+@Table(name="incomes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Income {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private BigInteger id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userCategory_id", nullable = false)
+    private UserCategory userCategory;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+    private String from;
+
+    @Column(nullable = false)
+    @NotBlank
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    @NotBlank
+    private Currency currency;
+
+    private LocalDateTime registeredAt;
+}
